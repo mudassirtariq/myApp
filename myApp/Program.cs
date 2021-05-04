d07ea14cd4a951a702912961557fadd2fa2f619f
mudassirtariq <79908927+mudassirtariq@users.noreply.github.com>
2021-05-04 14:53:36 +0500

    Update Program.cs

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -6,7 +6,7 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello this is Sample App");
 			Console.WriteLine("The current time is " + DateTime.Now);
         }
     }
