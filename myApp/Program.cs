56eb0d982b540f0e4af4523038acbe3d64592a96
mudassirtariq <mudassir.tariq@logicielservice.com>
2021-05-04 14:59:48 +0500

    added text my sample app

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -6,7 +6,7 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello this is Sample App");
+            Console.WriteLine("Hello this is my Sample App");
 			Console.WriteLine("The current time is " + DateTime.Now);
         }
     }
